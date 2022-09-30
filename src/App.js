7ba5f667d23ae7dc12de15a910f0f479070a057e
mazuh <marcell-mz@hotmail.com>
2022-09-30 09:54:53 -0300

    support many remote users

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ export default function App() {
           <>
             <Controls meetingSession={meetingSession} />
             <VideoLocalOutput meetingSession={meetingSession} />
-            <VideoRemoteOutput meetingSession={meetingSession} />
+            <RemoteVideosSection meetingSession={meetingSession} />
             <AudioOutput meetingSession={meetingSession} />
           </>
         )}
@@ -166,19 +166,30 @@ function JoiningMeeting({ onJoin }) {
 }
 
 function Controls({ meetingSession }) {
+  const muteAudio = () => meetingSession.audioVideo.realtimeMuteLocalAudio();
+
+  const unmuteAudio = () =>
+    meetingSession.audioVideo.realtimeUnmuteLocalAudio();
+
+  const muteVideo = () => meetingSession.audioVideo.stopVideoInput();
+
+  const unmuteVideo = async () => {
+    console.error("Not implemented yet!");
+  };
+
   return (
     <SectionBox heading="Controls">
-      <Button
-        type="button"
-        onClick={() => meetingSession.audioVideo.realtimeMuteLocalAudio()}
-      >
+      <Button type="button" onClick={muteAudio}>
         Mute audio
       </Button>
-      <Button
-        type="button"
-        onClick={() => meetingSession.audioVideo.realtimeUnmuteLocalAudio()}
-      >
+      <Button type="button" onClick={unmuteAudio}>
         Unmute audio
+      </Button>
+      <Button type="button" onClick={muteVideo}>
+        Mute video
+      </Button>
+      <Button type="button" onClick={unmuteVideo}>
+        Unmute video
       </Button>
       <Button
         type="button"
@@ -245,6 +256,7 @@ function VideoLocalOutput({ meetingSession }) {
   );
 }
 
+// eslint-disable-next-line
 function VideoRemoteOutput({ meetingSession }) {
   const videoRef = useRef(null);
 
@@ -277,11 +289,100 @@ function VideoRemoteOutput({ meetingSession }) {
   }, [meetingSession]);
 
   return (
-    <SectionBox heading="Video Remote Output">
+    <SectionBox heading="Video Remote Output (for 1-on-1s)">
       <PeerBox enabled>
         <Video ref={videoRef} />
       </PeerBox>
     </SectionBox>
+  );
+}
+
+function RemoteVideosSection({ meetingSession }) {
+  const videoSlotsRef = useRef(
+    Array(25)
+      .fill()
+      .map(() => ({ tileId: null, video: null }))
+  );
+
+  const [enabledTiles, setEnabledTiles] = useState([]);
+  const enableTile = (tileId) =>
+    setEnabledTiles((previous) => [...previous, tileId]);
+  const disableTile = (tileId) =>
+    setEnabledTiles((previous) => previous.filter((p) => p !== tileId));
+  const isEnabledTile = (tileId) => enabledTiles.includes(tileId);
+
+  useEffect(() => {
+    const findSlot = (tileId) =>
+      videoSlotsRef.current.find((slot) => slot.tileId === tileId) ||
+      videoSlotsRef.current.find((slot) => !slot.tileId);
+    const mapToAssignedSlot = (assigningTileId, assigningSlot) =>
+      videoSlotsRef.current.map((slot) =>
+        slot.video === assigningSlot.video
+          ? { ...slot, tileId: assigningTileId }
+          : slot
+      );
+    const mapToUnassignedSlot = (unassigningTileId) =>
+      videoSlotsRef.current.map((slot) =>
+        slot.tileId === unassigningTileId ? { ...slot, tileId: null } : slot
+      );
+
+    const mutateVideoSlotsRef = (updatingSlots) => {
+      videoSlotsRef.current = updatingSlots;
+    };
+
+    const observer = {
+      videoTileDidUpdate: (tileState) => {
+        if (
+          !tileState.boundAttendeeId ||
+          tileState.localTile ||
+          tileState.isContent
+        ) {
+          return;
+        }
+
+        const slot = findSlot(tileState.tileId);
+        if (!slot) {
+          throw new Error("Failed to find slot for remote peer.");
+        }
+
+        mutateVideoSlotsRef(mapToAssignedSlot(tileState.tileId, slot));
+
+        if (tileState.active) {
+          enableTile(tileState.tileId);
+        }
+
+        meetingSession.audioVideo.bindVideoElement(
+          tileState.tileId,
+          slot.video
+        );
+      },
+      videoTileWasRemoved: (tileId) => {
+        mutateVideoSlotsRef(mapToUnassignedSlot(tileId));
+        disableTile(tileId);
+      },
+    };
+
+    meetingSession.audioVideo.addObserver(observer);
+  }, [meetingSession]);
+
+  return (
+    <Box component="section">
+      <h3>Other users</h3>
+      {!enabledTiles.length && (
+        <Typography component="p">No remote peers have joined yet.</Typography>
+      )}
+      <Box>
+        {videoSlotsRef.current.map((slot, index) => (
+          <PeerBox
+            key={index}
+            title={index}
+            enabled={isEnabledTile(slot.tileId)}
+          >
+            <Video ref={(video) => (slot.video = video)} />
+          </PeerBox>
+        ))}
+      </Box>
+    </Box>
   );
 }
 
